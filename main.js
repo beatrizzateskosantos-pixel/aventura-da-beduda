b516056bea5392447803f108385ae8b40e0545c9
beatrizzateskosantos-pixel <beatriz.zatesko.santos@escola.pr.gov.br>
2025-10-30 09:29:09 -0300

    d

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListener('click',function(){
-
-    })
+  const atual = document.querySelector(`.ativo`)
+  const proximoPasso = "passo-" + this.getAttribute(`data-proxima`)
+  atual.classList.remove(`ativo`)
+  document.getElementById(proximoPasso).classList.add(`ativo`)
+    }) 
 }
 
 )
